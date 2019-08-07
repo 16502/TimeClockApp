bb09c6669203e18139142b9154efca021415147e
16502 <39684839+16502@users.noreply.github.com>
2019-08-08 09:26:53 +1200

    Add files via upload

---
+++ scene2.lua
@@ -0,0 +1,132 @@
+-- =============================================================
+-- 12DTS Composer Example
+-- =============================================================
+-- Scene 2
+-- =============================================================
+local composer 		= require( "composer" )
+local scene    		= composer.newScene()
+local widget 		= require( "widget" )
+----------------------------------------------------------------------
+--								LOCALS								--
+----------------------------------------------------------------------
+-- Constants
+local fullw = display.contentWidth
+local fullh = display.contentHeight
+local centerX = display.contentCenterX
+local centerY = display.contentCenterY
+
+----------------------------------------------------------------------
+--	Scene Methods
+----------------------------------------------------------------------
+
+----------------------------------------------------------------------
+----------------------------------------------------------------------
+function scene:create( event )
+
+	local sceneGroup = self.view
+	-- Place all your code in this section.
+	-- Make sure all display objects eventually end up in the screenGroup.
+	----------------------------------------------------------------------
+	----------------------------------------------------------------------
+	local title = display.newText("Scene 2", centerX, centerY, nil, 26)
+	sceneGroup:insert(title)
+	-- -- Function to handle button events
+	local function handleButtonEvent( event )
+
+		if ( "ended" == event.phase ) then
+			print( "Button was pressed and released" )
+			local options = {
+				effect = "flip",
+				time = 800
+			}
+			composer.gotoScene( "scenes.scene1",options )	
+		end
+	end
+
+	-- -- Create the widget
+	local button1 = widget.newButton(
+		{
+			label = "Goto Scene 1",
+			onEvent = handleButtonEvent,
+			emboss = false,
+			-- Properties for a rounded rectangle button
+			shape = "roundedRect",
+			width = 200,
+			height = 40,
+			cornerRadius = 2,
+			fillColor = { default={1,0,0,1}, over={1,0.1,0.7,0.4} },
+			strokeColor = { default={1,0.4,0,1}, over={0.8,0.8,1,1} },
+			strokeWidth = 4
+		}
+	)
+	sceneGroup:insert(button1)
+	-- Center the button
+	button1.x = centerX
+	button1.y = centerY + 100
+end
+
+---------------------------------------------------------------------------------
+-- Generally Do Not Touch Below This Line
+----------------------------------------------------------------------
+function scene:willEnter( event )
+	local sceneGroup = self.view
+end
+----------------------------------------------------------------------
+----------------------------------------------------------------------
+function scene:didEnter( event )
+	local sceneGroup = self.view
+	local prevScene = composer.getSceneName( "previous" )
+	if prevScene then
+		composer.removeScene( prevScene, true ) 
+		print("removing ", prevScene)
+	end
+end
+
+----------------------------------------------------------------------
+----------------------------------------------------------------------
+function scene:willExit( event )
+	local sceneGroup = self.view
+end
+----------------------------------------------------------------------
+----------------------------------------------------------------------
+function scene:didExit( event )
+	local sceneGroup = self.view
+end
+
+----------------------------------------------------------------------
+----------------------------------------------------------------------
+function scene:destroy( event )
+	local sceneGroup = self.view
+end
+
+----------------------------------------------------------------------
+--				FUNCTION/CALLBACK DEFINITIONS						--
+----------------------------------------------------------------------
+
+---------------------------------------------------------------------------------
+-- Scene Dispatch Events, Etc. - Generally Do Not Touch Below This Line
+---------------------------------------------------------------------------------
+function scene:show( event )
+	local sceneGroup 	= self.view
+	local willDid 	= event.phase
+	if( willDid == "will" ) then
+		self:willEnter( event )
+	elseif( willDid == "did" ) then
+		self:didEnter( event )
+	end
+end
+function scene:hide( event )
+	local sceneGroup 	= self.view
+	local willDid 	= event.phase
+	if( willDid == "will" ) then
+		self:willExit( event )
+	elseif( willDid == "did" ) then
+		self:didExit( event )
+	end
+end
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+---------------------------------------------------------------------------------
+return scene
